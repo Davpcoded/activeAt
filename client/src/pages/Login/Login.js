98675fa8fd0e127bfb47eb10fa11dc9020f023d1
Darryl <williams.de39@gmail.com>
2020-08-01 14:20:21 -0700

    sign up

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -115,7 +115,7 @@ export default function SignInSide() {
                 </Link>
               </Grid>
               <Grid item>
-                <Link href="#" variant="body2">
+                <Link href="/signup" variant="body2">
                   {"Don't have an account? Sign Up"}
                 </Link>
               </Grid>
