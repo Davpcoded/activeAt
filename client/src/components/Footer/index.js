69b65214d9d0b73fd69902fc67e33253e1bfffba
Sveta Demkovich <svetlana.demkovich@gmail.com>
2020-08-01 13:59:32 -0700

    add Activitylist page and navbar nad footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,26 +1,29 @@
 import React from "react";
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles((theme) => ({
+    footer: {
+      backgroundColor: theme.palette.background.paper,
+      padding: theme.spacing(6),
+    },
+  }));
+
 
 function Footer() {
+    const classes = useStyles();
     return (
-        <div>
-            <p><i className="far fa-copyright"></i>"Copyright BCS Bootcamp"</p>
-            <p><i className="far fa-heart"></i>Thoughtfully created by <a href="">Cameron DeChand</a>,
-              <a href="">Daniel Villarroel</a>, <a href="">Darryl Williams</a>,
-               <a href="">Nathan Bowdish</a> and <a href="">Sveta Demkovich</a></p>
-        </div>
+<footer className={classes.footer}>
+<Typography variant="subtitle1" align="center" gutterBottom>
+Copyright BCS Bootcamp
+</Typography>
+<Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+Thoughtfully created by Cameron DeChand, Daniel Villarroel, Darryl Williams, Nathan Bowdish, Sveta Demkovich
+</Typography>
+
+</footer>
     );
 }
 
 export default Footer;
-
-
-
-<div class="container hide">
-    <div class="row">
-        <footer>
-            <p><i class="far fa-copyright"></i>Copyright BCS Bootcamp</p>
-            <p><i class="far fa-heart"></i> Thoughtfully created by <a href="">Cameron DeChand</a>,
-              <a href="">Daniel Villarroel</a>, <a href="">Darryl Williams</a>, <a href="">Nathan Bowdish</a> and <a href="">Sveta Demkovich</a></p>
-        </footer>
-    </div>
-</div>
