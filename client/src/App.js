14d40f14161b49e71a992061824aa79273562a46
Darryl <williams.de39@gmail.com>
2020-08-02 16:01:20 -0700

    added landing page, committed out the maps, so app can work

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,18 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import Navbar from "./components/Navbar/index";
 import SignUp from "./pages/SignUp/signUp";
-import Home from './components/Home/Home'
+//import Home from './components/Home/Home'
+import Landing from "./pages/Landing/index";
 
 function App() {
   return (
     <Router>
       <div>
         <Navbar />
+        <Route exact path="/" component={Landing} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/signup" component={SignUp} />
-        <Home />
+        {/* <Home /> */}
       </div>
     </Router>
   );
