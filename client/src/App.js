ac2661b3e901b2d470370792a3204809f9f56809
Sveta Demkovich <svetlana.demkovich@gmail.com>
2020-08-04 20:03:57 -0700

    updated API page with axios

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar/index";
 import SignUp from "./pages/SignUp/signUp";
 //import Home from './components/Home/Home'
 import Landing from "./pages/Landing/index";
+import Activitylist from "./pages/Activity/ActivityList"
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route exact path="/" component={Landing} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/signup" component={SignUp} />
+        <Route exact path="/activity" component={Activitylist} />
         {/* <Home /> */}
       </div>
     </Router>
