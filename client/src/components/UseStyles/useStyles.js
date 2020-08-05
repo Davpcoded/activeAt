074a999d667cf9d34a1f5a0f533e38cbe1839913
C1DeChand <C1DeChand@gmail.com>
2020-08-04 19:37:44 -0700

    Base Event Creation Page

---
+++ client/src/components/UseStyles/useStyles.js
@@ -0,0 +1,23 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+      border: 0,
+      borderRadius: 3,
+      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+      color: 'white',
+      height: 48,
+      padding: '0 30px',
+    },
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+    
+  }));
+
+  export default useStyles;
