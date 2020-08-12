db3871ec75ba172d6a9d546ad76938166f5d8fed
Darryl <williams.de39@gmail.com>
2020-08-11 20:30:15 -0700

    cleaned up landing page. made card component

---
+++ client/src/components/Cards/index.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+import Container from "@material-ui/core/Container";
+import useStyles from "../../components/UseStyles/useStyles";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import API from "../../utils/API";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+export default function AvailableEvents() {
+  const classes = useStyles();
+
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    loadEvents();
+  }, []);
+
+  function loadEvents() {
+    API.getEvents()
+      .then((res) => setEvents(res.data))
+      .catch((err) => console.log(err));
+  }
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <main>
+        <Container className={classes.cardGrid} maxWidth="md">
+          <Grid container spacing={4}>
+            {events.map((event) => (
+              <Grid item key={event._id} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image={
+                      "https://source.unsplash.com/search/photos?query=" +
+                        event.eventType || "https://source.unsplash.com/photos"
+                    }
+                    title={event.eventName}
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {event.eventName}
+                    </Typography>
+                    <Typography>{event.eventType}</Typography>
+                    <Typography>{event.eventDescription}</Typography>
+                    <Typography>{event.markerPositon}</Typography>
+                    <Typography>{event.eventDate}</Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      View
+                    </Button>
+                    <Button size="small" color="primary">
+                      Join Event
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+    </React.Fragment>
+  );
+}
